fb287e476abc12703a9b586e80013ccaaff0a702
ahfary <afabqory@gmail.com>
2024-10-04 13:20:04 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='min-h-screen flex justify-center items-center'>
+      <h1 className='text-3xl font-bold'>Ini Qoy Belajar Next JS</h1>
+    </div>
+  )
+}
+
+export default Page
